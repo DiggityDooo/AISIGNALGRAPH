47ce24b372d8b2d4494d881be4b7e84a5ffcb809
Sean <seanb@example.com>
2026-05-04 03:04:43 -0700

    fix: enable CORS for local development on localhost

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -104,6 +104,16 @@ def create_app() -> Flask:
 
         return decorator
 
+    @app.after_request
+    def add_cors_headers(response):
+        origin = request.headers.get('Origin')
+        if origin and 'localhost' in origin:
+            response.headers['Access-Control-Allow-Origin'] = origin
+            response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+            response.headers['Access-Control-Allow-Headers'] = 'Content-Type, X-CSRF-Token'
+            response.headers['Access-Control-Allow-Credentials'] = 'true'
+        return response
+
     @app.before_request
     def protect_post_routes():
         if request.method in {"POST", "PUT", "PATCH", "DELETE"}:
